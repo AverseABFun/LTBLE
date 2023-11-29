dedba9595aa92c3a7e34d6f4f28d9fb334a4625d
AverseABFun <averse.abfun@gmail.com>
2023-11-29 11:06:10 -0600

    rgtfdshjufrjhndjklnd

--- compiler.js
+++ compiler.js
@@ -1,7 +1,56 @@
 'use strict';
 
-function tokenize(code) {
+let keywords = [
+    "include",
+    "lang",
+    "program",
+    "on",
+    "in",
+    "of"
+];
 
+function tokenize(code) {
+    let cursor = 0;
+
+    let tokens = [];
+
+    while (current < code.length) {
+        let char = code[current];
+
+        if (char === ';') {
+            current++;
+
+            continue;
+        }
+
+        if (char === "{") {
+            tokens.push({
+                type: 'brace',
+                value: "{"
+            });
+
+            current++;
+
+            continue;
+        }
+
+        if (char === "}") {
+            tokens.push({
+                type: 'brace',
+                value: "}"
+            });
+
+            current++;
+
+            continue;
+        }
+
+        let WHITESPACE = /\s/;
+        if (WHITESPACE.test(char)) {
+            current++;
+            continue;
+        }
+    }
 }
 
 function parse(tokens) {
